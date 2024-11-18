2e0781ba0151e62562395db5e7866978647bb4c1
Eva_025 <94.05.25jj@gmail.com>
2024-11-18 16:33:08 +0800

    first

---
+++ app/src/main/java/com/example/game2d/Game.kt
@@ -0,0 +1,23 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+
+
+class Game(val scope: CoroutineScope) {
+
+    var counter = 0
+    val state = MutableStateFlow(0)
+
+    fun Play(){
+        scope.launch {
+            counter = 0
+            while (counter<5000) {
+                delay(40)
+                counter++
+                state.emit(counter)
+
+            }
+        }
+    }
+}
